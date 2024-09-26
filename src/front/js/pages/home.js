1ea1971d93937a92596ff812c53b0498cc5dc548
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-09-26 13:54:36 +0000

    alt de imagenes añadidas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -15,7 +15,7 @@ export const Home = () => {
 			<div class="row row-cols-1 row-cols-md-3 g-4">
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://media.istockphoto.com/id/1414016924/photo/white-mockup-mug-with-copy-space-and-houseplant-at-the-background.jpg?s=612x612&w=0&k=20&c=2CCGUMWlr3yJrHGtYAo1ZDPChVfoWRZtAmD9qhwjPko=" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://media.istockphoto.com/id/1414016924/photo/white-mockup-mug-with-copy-space-and-houseplant-at-the-background.jpg?s=612x612&w=0&k=20&c=2CCGUMWlr3yJrHGtYAo1ZDPChVfoWRZtAmD9qhwjPko=" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cozy White Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Cozy White Mug</h5>
 				<p class="card-text">Experience the ultimate in comfort and style with the Cozy White Mug! This beautifully designed mug embodies simplicity and elegance, making it a perfect addition to any kitchen or office.</p>
@@ -24,7 +24,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://media.istockphoto.com/id/171368204/photo/red-cup.jpg?s=612x612&w=0&k=20&c=q0CQtZJAGs8VXm2B7462QDJ0tlk18h1EPApvvZNgBus=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="..."/>
+			<img src="https://media.istockphoto.com/id/171368204/photo/red-cup.jpg?s=612x612&w=0&k=20&c=q0CQtZJAGs8VXm2B7462QDJ0tlk18h1EPApvvZNgBus=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="Crimson Red Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Crimson Red Mug</h5>
 				<p class="card-text">Ignite your passion for great coffee and tea with the Crimson Red Mug! This vibrant mug is more than just a drinkware piece—it's a bold statement.</p>
@@ -33,7 +33,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://media.istockphoto.com/id/869295342/photo/image-of-coffee-cup-isolated-with-clipping-path.jpg?s=612x612&w=0&k=20&c=AWQmOCfkjecoezwGk-jFSWKEO6xRzBnM_Jkj5LcD0oU=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="..."/>
+			<img src="https://media.istockphoto.com/id/869295342/photo/image-of-coffee-cup-isolated-with-clipping-path.jpg?s=612x612&w=0&k=20&c=AWQmOCfkjecoezwGk-jFSWKEO6xRzBnM_Jkj5LcD0oU=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="Verdant Green Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Verdant Green Mug</h5>
 				<p class="card-text">Brighten your day with the Verdant Green Mug, a perfect blend of nature-inspired charm and functionality. This beautifully crafted mug showcases a rich green hue that evokes a sense of tranquility and rejuvenation.</p>
@@ -42,7 +42,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://img.freepik.com/premium-photo/color-burst-chaos-coffee-mug-mockup-brand-merchandise_901408-7480.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://img.freepik.com/premium-photo/color-burst-chaos-coffee-mug-mockup-brand-merchandise_901408-7480.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cream Elegance Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Cream Elegance Mug</h5>
 				<p class="card-text">Introducing the Cream Elegance Mug, a timeless piece that embodies sophistication and warmth. This beautifully crafted mug features a soft cream color that complements any decor.</p>
@@ -51,7 +51,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://media.istockphoto.com/id/617374430/photo/pink-ceramic-cup-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=TBBMp_4y1_K10vTn2Uv81RjA7I43sZv4Z3pxtb9Xq0A=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="..."/>
+			<img src="https://media.istockphoto.com/id/617374430/photo/pink-ceramic-cup-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=TBBMp_4y1_K10vTn2Uv81RjA7I43sZv4Z3pxtb9Xq0A=" class="card-img-top" style={{ width: "250px", height: "220px"}} alt="Blush Pink Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Blush Pink Mug</h5>
 				<p class="card-text">Elevate your sipping experience with the charming Blush Pink Mug! This delightful mug is a perfect blend of style and functionality, designed to bring a touch of elegance to your daily routine</p>
@@ -60,7 +60,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://media.istockphoto.com/id/184982694/photo/classic-mug.jpg?s=612x612&w=0&k=20&c=o18BD0m6p4Bc1u5tcIO63iCiRZ4HlxgouOAUQHC98cQ=" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://media.istockphoto.com/id/184982694/photo/classic-mug.jpg?s=612x612&w=0&k=20&c=o18BD0m6p4Bc1u5tcIO63iCiRZ4HlxgouOAUQHC98cQ=" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Sunset Orange Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Sunset Orange Mug</h5>
 				<p class="card-text">Brighten up your day with the Sunset Orange Mug! This vibrant mug is designed to add a pop of color to your morning routine or afternoon break.</p>
@@ -69,7 +69,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://img.freepik.com/premium-photo/white-coffee-mug-wooden-table-with-words-o-side_1058796-39492.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://img.freepik.com/premium-photo/white-coffee-mug-wooden-table-with-words-o-side_1058796-39492.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cozy Sip Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Cozy Sip Mug</h5>
 				<p class="card-text">Introducing the Cozy Sip Mug, designed with your comfort in mind. This mug combines style and functionality to create the ultimate sipping experience.</p>
@@ -78,7 +78,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://www.shutterstock.com/image-photo/yellow-ceramic-mug-isolated-on-600nw-2224244583.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://www.shutterstock.com/image-photo/yellow-ceramic-mug-isolated-on-600nw-2224244583.jpg" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Kiyomi Mug<"/>
 			<div class="card-body">
 				<h5 class="card-title">Kiyomi Mug</h5>
 				<p class="card-text">Introducing the Kiyomi Mug, a tribute to the elegance and tradition of Japanese craftsmanship. Inspired by timeless Japanese design.</p>
@@ -87,7 +87,7 @@ export const Home = () => {
 		</div>
 		<div class="col">
 			<div class="card h-100">
-			<img src="https://parkstreetbooks.com/cdn/shop/files/C40004_20_02_G_001_829x828_jpg_829x.webp?v=1701032167" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="..."/>
+			<img src="https://parkstreetbooks.com/cdn/shop/files/C40004_20_02_G_001_829x828_jpg_829x.webp?v=1701032167" class="card-img-top" style={{ width: "295px", height: "220px"}} alt="Luxe Mug"/>
 			<div class="card-body">
 				<h5 class="card-title">Luxe Mug</h5>
 				<p class="card-text">Introducing the Luxe Mug, where elegance meets everyday indulgence. Crafted with precision, this mug is more than just a vessel—it's a statement..</p>
