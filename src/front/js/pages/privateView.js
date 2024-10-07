6dec74f13c52b9d42d5426ca4a101c0519fb832a
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-10-07 20:50:20 +0000

    .

--- src/front/js/pages/privateView.js
+++ src/front/js/pages/privateView.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export const PrivateView = () => {
     const navigate = useNavigate ();
@@ -21,9 +21,48 @@ const handleLogOut = () => {
     navigate('/LogIn');
 };
 
+// // CART ARRAY
+
+// let cart = [];
+
+// // ADD CART
+
+// function addToCart(id) {
+// // Checking if the product already exists in the cart 
+
+// 	if (cart.some((item) => item.id === id)){
+// 		alert("Product already in cart!");
+// 	} else {
+// 	const item = products.find((product) => product.id === id);
+// 	cart.push({
+// 		...item,
+// 		numberOfUnits:1,
+// 	});
+// }
+
+// updateCart();
+
+// }
+
+// //UPDATE CART
+// function updateCart() {
+// 	renderCartItems();
+// 	// renderSubtotal();
+// }
+
+// //Render Cart Items
+
+// function renderCartItems() {
+// 	cart.forEach(() => {
+// 		cartItemsEl.innerHTML += 
+// 	})
+// }
+
+
+
 return (
     <div className="home">
-    <div className="container">
+	<div className="container">
         <nav className="navbar">
         <div style={{ display: 'flex', alignItems: 'center' }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="45" height="25" fill="black" className="bi bi-cup-hot-fill" viewBox="0 0 16 16">
@@ -35,56 +74,54 @@ return (
             </span>
         </div>
             <div d-flex="true" gap-2="true">
-                <div>     
-                    <button onClick={handleLogOut} className="btn btn-danger" style={{fontSize:"12px", padding:"5px 10px"}}> Log Out </button> 
+                <div>   
+					{/* <button className="btn btn-dark" style={{ fontSize:"12px", padding:"5px 10px" }}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+						<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4z"/>
+						</svg>
+					</button>  */}
+                    <button onClick={handleLogOut} className="btn btn-danger" style={{ fontSize:"12px", padding:"5px 10px", margin:"10px" }}> Log Out </button> 
                 </div>
             </div>
         </nav>
         <div>
             <h1>Welcome!</h1>
         </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4">
+		<div className="row row-cols-1 row-cols-md-3" style={{ gap: "10px 10px" }}>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://media.istockphoto.com/id/1414016924/photo/white-mockup-mug-with-copy-space-and-houseplant-at-the-background.jpg?s=612x612&w=0&k=20&c=2CCGUMWlr3yJrHGtYAo1ZDPChVfoWRZtAmD9qhwjPko=" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cozy White Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Cozy White Mug</h5>
 					<p className="card-text">Experience the ultimate in comfort and style with the Cozy White Mug! 
 						This beautifully designed mug embodies simplicity and elegance, making it a perfect addition to any kitchen or office.</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>7.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>7.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
-				
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://media.istockphoto.com/id/171368204/photo/red-cup.jpg?s=612x612&w=0&k=20&c=q0CQtZJAGs8VXm2B7462QDJ0tlk18h1EPApvvZNgBus=" className="card-img-top" style={{ width: "250px", height: "220px"}} alt="Crimson Red Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Crimson Red Mug</h5>
 					<p className="card-text">Ignite your passion for great coffee and tea with the Crimson Red Mug! 
 						This vibrant mug is more than just a drinkware piece—it's a bold statement. Hurry and dont miss out on this oportunity!</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>4.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>4.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://media.istockphoto.com/id/869295342/photo/image-of-coffee-cup-isolated-with-clipping-path.jpg?s=612x612&w=0&k=20&c=AWQmOCfkjecoezwGk-jFSWKEO6xRzBnM_Jkj5LcD0oU=" className="card-img-top" style={{ width: "250px", height: "220px"}} alt="Verdant Green Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Verdant Green Mug</h5>
@@ -92,38 +129,32 @@ return (
 						a perfect blend of nature-inspired charm and functionality. 
 						This beautifully crafted mug showcases a rich green hue.</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>6.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>6.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://img.freepik.com/premium-photo/color-burst-chaos-coffee-mug-mockup-brand-merchandise_901408-7480.jpg" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cream Elegance Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Cream Elegance Mug</h5>
 					<p className="card-text">Introducing the Cream Elegance Mug, a timeless piece that embodies sophistication and warmth. 
 						This beautifully crafted mug features a soft cream color that complements any decor.</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>10.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>10.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://media.istockphoto.com/id/617374430/photo/pink-ceramic-cup-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=TBBMp_4y1_K10vTn2Uv81RjA7I43sZv4Z3pxtb9Xq0A=" className="card-img-top" style={{ width: "250px", height: "220px"}} alt="Blush Pink Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Blush Pink Mug</h5>
@@ -131,76 +162,64 @@ return (
 						This delightful mug is a perfect blend of style and functionality, 
 						designed to bring a touch of elegance to your daily routine</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>5.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>5.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://media.istockphoto.com/id/1146776554/fr/photo/gros-plan-de-tasse-%C3%A0-vapeur-de-caf%C3%A9-sur-comptoir-de-cuisine-en-d%C3%A9but-de-matin%C3%A9e.jpg?s=612x612&w=0&k=20&c=MwezknFHCfrKL2i8KEv8WZGU8KqSVSmJp9UwrMT3yIs=" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Sunset Orange Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Sunset Orange Mug</h5>
 					<p className="card-text">Brighten up your day with the Sunset Orange Mug! 
 						This vibrant mug is designed to add a pop of color to your morning routine or afternoon break. Hurry and dont miss this oportunity.</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>7.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>7.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://img.freepik.com/premium-photo/white-coffee-mug-wooden-table-with-words-o-side_1058796-39492.jpg" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Cozy Sip Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Cozy Sip Mug</h5>
 					<p className="card-text">Introducing the Cozy Sip Mug, designed with your comfort in mind. 
 						This mug combines style and functionality to create the ultimate sipping experience.</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>11.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>11.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://www.shutterstock.com/image-photo/yellow-ceramic-mug-isolated-on-600nw-2224244583.jpg" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Kiyomi Mug<"/>
 				<div className="card-body">
 					<h5 className="card-title">Kiyomi Mug</h5>
 					<p className="card-text">Introducing the Kiyomi Mug, a tribute to the elegance and tradition of Japanese craftsmanship. 
 						Inspired by timeless Japanese design. Hurry so you dont miss out!</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>15.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>15.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark">Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
 			</div>
 			<div className="col">
-				<div className="card h-100">
+				<div className="card h-100" style={{ width: "300px" }}>
 				<img src="https://parkstreetbooks.com/cdn/shop/files/C40004_20_02_G_001_829x828_jpg_829x.webp?v=1701032167" className="card-img-top" style={{ width: "295px", height: "220px"}} alt="Luxe Mug"/>
 				<div className="card-body">
 					<h5 className="card-title">Luxe Mug</h5>
@@ -208,13 +227,10 @@ return (
 						Crafted with precision, this mug is more than just a vessel—it's a statement.. You wouldn'
 						want to miss out!</p>
 					<div>
-						<p style={{fontSize:'20px'}}><strong>10.99$</strong></p>
+						<p style={{ fontSize:'20px' }}><strong>10.99$</strong></p>
 					</div>
                     <div className="button-container">
                         <button type="button" class="btn btn-dark"> Add to Cart</button>
-                        <button type="button" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  						<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
-						</button>
                     </div>
 				</div>
 				</div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,40 +1,10 @@
 const getState = ({ getStore, setStore }) => {
+	
 	return {
-		store: {
-		  favorites: JSON.parse(localStorage.getItem("favorites")) || [],
-		},
+		// store: {
+		//   favorites: JSON.parse(localStorage.getItem("favorites")) || [],
+		// },
 		actions: {
-		  addToFavorites: (item, type) => {
-			const store = getStore();
-			const favorites = store.favorites;
-	  
-			// Create a unique identifier for the favorite item
-			const uniqueId = `${item.uid}-${type}`;
-	  
-			// Check if the item is already a favorite
-			const isAlreadyFavorite = favorites.some(favorite => favorite.uniqueId === uniqueId);
-	  
-			// If it's already a favorite, remove it; otherwise, add it
-			const newFavorites = isAlreadyFavorite 
-			  ? favorites.filter(favorite => favorite.uniqueId !== uniqueId)
-			  : [...favorites, { ...item, type, uniqueId }];
-	  
-			// Update the store and persist the favorites to localStorage
-			setStore({ favorites: newFavorites });
-			localStorage.setItem("favorites", JSON.stringify(newFavorites));
-		  },
-	  
-		  // Function to initialize the favorites from localStorage
-		  getFavorites: () => {
-			try {
-			  const storedFavorites = localStorage.getItem("favorites");
-			  if (storedFavorites) {
-				setStore({ favorites: JSON.parse(storedFavorites) });
-			  }
-			} catch (error) {
-			  console.log("Error getting favorites", error);
-				}
-			},
 
 			getMessage: async () => {
 				try{
@@ -47,20 +17,6 @@ const getState = ({ getStore, setStore }) => {
 				}catch(error){
 					console.log("Error loading message from backend", error)
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			},
 		},
 	};
